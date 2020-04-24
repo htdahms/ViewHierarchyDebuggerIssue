a0bd6ec660817badc44fd03aa92866c31f8e763c
Heinrich Dahms <heinrich.dahms@redspace.com>
2020-04-24 14:37:33 -0300

    Initial Commit

---
+++ ViewHierarchyIssueExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewHierarchyIssueExample
+//
+//  Created by Heinrich Dahms on 2020-04-24.
+//  Copyright © 2020 Heinrich Dahms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
